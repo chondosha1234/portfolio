6d778515d49e57f37947054b56739bf51841039b
chondosha1234 <jmiller61193@gmail.com>
2023-02-03 09:17:59 -0500

    removed old function views

--- accounts/views.py
+++ accounts/views.py
@@ -9,28 +9,11 @@ from django.views.generic import View
 
 User = get_user_model()
 
-def login(request):
-    if request.POST:
-        email = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(email=email, password=password)
-        if user:
-            auth_login(request, user)
-            return redirect('home')
-    return render(request, 'login.html')
 
 def logout(request):
     auth_logout(request)
     return redirect('home')
 
-def create_account(request):
-    if request.POST:
-        if not request.POST['password'] or not request.POST['confirm_password']:
-            return redirect('create_account')
-        else:
-            new_user = User.objects.create(email=request.POST['email'], password=request.POST['password'])
-            return redirect('login')
-    return render(request, 'create_account.html')
 
 class LoginView(View):
     template_name = "login.html"
@@ -52,6 +35,7 @@ class LoginView(View):
                 auth_login(request, user)
                 return redirect('home')
         return render(request, self.template_name)
+
 
 class CreateAccountView(View):
     template_name = "create_account.html"
