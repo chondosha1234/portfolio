c0aabedd1d5109ee4b476e737b53c0b26ae7113c
chondosha1234 <jmiller61193@gmail.com>
2023-02-09 14:37:20 -0500

    added css styling to pages

---
+++ tictactoe/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+class TicTacToeViewsTest(TestCase):
+
+    def test_renders_start_page(self):
+        response = self.client.get('/tictactoe/')
+        self.assertEquals(response.templates[0].name, 'tictactoe_start.html')
+        self.assertTemplateUsed(response, 'tictactoe_start.html')
+
+    def test_POST_renders_game_page(self):
+        response = self.client.post('/tictactoe/game', data={
+            "textx": "Jon",
+            "texto": "Dasha"
+        })
+        self.assertEquals(response.templates[0].name, 'tictactoe.html')
+        self.assertTemplateUsed(response, 'tictactoe.html')
